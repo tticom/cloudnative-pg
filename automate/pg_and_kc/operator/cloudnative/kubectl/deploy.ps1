896b9d1884d9c80a8d3ba023114c41764d5db67c
tticom <niall.finlay@gmail.com>
2024-11-15 14:02:38 +0000

    Rationalised to just deploy.ps1 files

--- automate/pg_and_kc/operator/cloudnative/kubectl/deploy.ps1
+++ automate/pg_and_kc/operator/cloudnative/kubectl/deploy.ps1
@@ -9,31 +9,31 @@ kubectl apply -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg
 kubectl get deploy -n cnpg-system cnpg-controller-manager
 
 # get more information using the describe command in
-kubectl get deployments -n cnpg-system
+# kubectl get deployments -n cnpg-system
 # kubectl describe deploy -n cnpg-system <deployment-name>
-kubectl describe deploy -n cnpg-system cnpg-controller-manager 
+# kubectl describe deploy -n cnpg-system cnpg-controller-manager 
 
 # https://cloudnative-pg.io/documentation/1.18/quickstart/#part-2-install-cloudnativepg
 
 # Apply the cluster manifest in base\postgres-cluster\postgres-cluster.yaml
 kubectl apply -f .\base\postgres-cluster\postgres-cluster.yaml
 
-kubectl get pods
+# kubectl get pods
 
 # https://cloudnative-pg.io/documentation/1.18/connection_pooling/
-kubectl get namespaces
+# kubectl get namespaces
 # namespace created by the cloudnative operator
-kubectl get pods -n cnpg-system
+# kubectl get pods -n cnpg-system
 
 # All other pods should be in default
-kubectl get pods -n default
+# kubectl get pods -n default
 
 # The Pooler must live in the same namespace of the Postgres cluster
 # If you specify your own secrets the operator will not automatically integrate the Pooler.
 
 kubectl apply -f .\base\postgres-cluster\pooler.yaml
 
-kubectl exec -ti <PGBOUNCER_POD> -- curl 127.0.0.1:9127/metrics
-kubectl exec -ti pooler-postgres-db-cluster-rw-f59cbd8d7-tqsjg -- curl 127.0.0.1:9127/metrics
-kubectl exec -ti pooler-postgres-db-cluster-rw-f59cbd8d7-w4kq2 -- curl 127.0.0.1:9127/metrics
-kubectl exec -ti pooler-postgres-db-cluster-rw-f59cbd8d7-j2sls -- curl 127.0.0.1:9127/metrics
+# kubectl exec -ti <PGBOUNCER_POD> -- curl 127.0.0.1:9127/metrics
+# kubectl exec -ti pooler-postgres-db-cluster-rw-f59cbd8d7-tqsjg -- curl 127.0.0.1:9127/metrics
+# kubectl exec -ti pooler-postgres-db-cluster-rw-f59cbd8d7-w4kq2 -- curl 127.0.0.1:9127/metrics
+# kubectl exec -ti pooler-postgres-db-cluster-rw-f59cbd8d7-j2sls -- curl 127.0.0.1:9127/metrics
--- automate/pg_and_kc/operator/keycloak/operator.ps1
+++
@@ -1,1 +0,0 @@
-kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/kubernetes.yml
