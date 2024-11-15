075f9602df3fd35765ba9d43d2f526c9433d2f07
niall.finlay <niall.finlay@blackfordanalysis.com>
2024-11-15 17:07:04 +0000

    alternate method of creating keyclaok instance (which seems to work)

--- automate/pg_and_kc/operator/keycloak/deploy.ps1
+++ automate/pg_and_kc/operator/keycloak/deploy.ps1
@@ -1,11 +1,15 @@
 
+minikube addons enable ingress
+
 # Install CRDs
+# kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
+# kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
 
-kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
-kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
+# # Create an instance of keycloak
+# kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/kubernetes.yml
+# kubectl apply -f ..\..\base\keycloak\keycloak-instance.yaml
 
-# Create an instance of keycloak
-kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/kubernetes.yml
-kubectl apply -f ..\..\base\keycloak\keycloak-instance.yaml
+kubectl create -f https://raw.githubusercontent.com/keycloak/keycloak-quickstarts/latest/kubernetes/keycloak.yaml
+
 kubectl apply -f ..\..\base\keycloak\realm.yaml
 
