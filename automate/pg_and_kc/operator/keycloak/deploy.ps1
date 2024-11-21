0a63ff0fb97833ac8cc18ed8866151aec845bde1
niall.finlay <niall.finlay@blackfordanalysis.com>
2024-11-21 11:46:25 +0000

    certs being created, to ingresses also to do with the deployment

--- automate/pg_and_kc/operator/keycloak/deploy.ps1
+++ automate/pg_and_kc/operator/keycloak/deploy.ps1
@@ -3,16 +3,44 @@ $Error.Clear()
 # Enable Minikube ingress addon
 minikube addons enable ingress
 
+# not required if using minikube 
+# https://cert-manager.io/docs/tutorials/acme/nginx-ingress/#step-2---deploy-the-nginx-ingress-controller
+# Add the latest helm repository for the ingress-nginx
+# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+# helm repo update
+# helm install quickstart ingress-nginx/ingress-nginx
+
+# https://cert-manager.io/docs/installation/helm/
+# Add the Helm repository
+helm repo add jetstack https://charts.jetstack.io --force-update
+# Install cert-manager
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.16.1 --set crds.enabled=true
+
+# https://cert-manager.io/docs/tutorials/acme/nginx-ingress/#step-2---deploy-the-nginx-ingress-controller
+# install the issuers
+# Only for testting because the prod issuer has severe usuage restrictions on prod
+kubectl apply -f ..\..\base\keycloak\staging-issuer.yaml
+# kubectl apply -f ..\..\base\keycloak\prod-issuer.yaml
+
+# kubectl get svc
+  
+# Write-Host "Waiting for 60 seconds to ensure ingress-nginx is installed..."
+# Start-Sleep -Seconds 60
+
 # Get Minikube IP and construct Keycloak URL
-$minikubeIp = & minikube ip
-$keycloakUrl = "keycloak.$minikubeip.nip.io"
-$yamlUrl = "https://raw.githubusercontent.com/keycloak/keycloak-quickstarts/latest/kubernetes/keycloak-ingress.yaml"
+# $minikubeIp = & minikube ip
+# $keycloakUrl = keycloak.192.168.49.2.nip.io
+# keycloak.192.168.49.2.nip.io needs to be mapped to 127.0.0.1 in the c:\windows\system32\drivers\etc\hosts file
+# $keycloakUrl = "keycloak.$minikubeip.nip.io"
+# $keycloakUrl = "local.keycloak.nip.io"
+$keycloakUrl = "local.keycloak.io"
+$ingressYaml = "..\..\base\keycloak\keycloak-ingress.yaml"
 
 try {
 
-# Install CRDs
-kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
-kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
+  # Install CRDs
+  kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
+  kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
 } catch {
   Write-Error "Failed to install keycloak CRDs: $_"
 }
@@ -24,29 +52,34 @@ try {
   # kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/26.0.5/kubernetes/kubernetes.yml
 
   kubectl apply -f ..\..\base\keycloak\keycloak-instance.yaml
+  
+  # Write-Host "Waiting for 60 seconds to ensure keycloak instance is ready..."
+  # Start-Sleep -Seconds 60
+
   kubectl apply -f https://raw.githubusercontent.com/keycloak/keycloak-quickstarts/latest/kubernetes/keycloak.yaml
   kubectl apply -f ..\..\base\keycloak\realm.yaml
 
-  (Invoke-WebRequest -Uri $yamlUrl -UseBasicParsing).Content -replace "KEYCLOAK_HOST", $keycloakUrl | kubectl apply -f -
+  (Get-Content -Path $ingressYaml) -replace "KEYCLOAK_HOST", $keycloakUrl | kubectl apply -f -
+
+  # Install cert-manager
+  # kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.1/cert-manager.yaml
+  
+  # Write-Host "Waiting for 60 seconds to ensure cert-manager is installed..."
+  # Start-Sleep -Seconds 60
+
+
+
 } catch {
   Write-Error "An error occurred: $_\n"
   exit 1
 }
 
-# (Invoke-WebRequest -Uri $yamlUrl -UseBasicParsing).Content -replace "KEYCLOAK_HOST", $keycloakUrl > ..\..base\keycloak\offline\keycloak-ingress.yaml
-
-# kubectl create -f ..\..base\keycloak\offline\keycloak-ingress.yaml
-
 Write-Host "Keycloak:               $keycloakUrl"
 Write-Host "Keycloak:               $keycloakUrl/admin"
-Write-Host "Keycloak:               $keycloakUrl/realms"
+Write-Host "Keycloak:               $keycloakUrl/realms/Blackford-Platform/account"
 
 if ($Error.count -gt 0){
   Write-Host "One or more error occurred during execution. Try running the script again and checking the logs."
   exit 1
-} else {
-  Write-Host "Keycloak deployment successful, check the running pods"
-  Write-Host "Starting minikube tunnel..."
-  minikube tunnel
 }
 
--- automate/pg_and_kc/readme.md
+++ automate/pg_and_kc/readme.md
@@ -4,6 +4,7 @@
 minikube - https://minikube.sigs.k8s.io/docs/start/
 kubectl - https://kubernetes.io/docs/tasks/tools/
 helm - https://helm.sh/docs/intro/install/
+cmctl - https://cert-manager.io/docs/installation/kubectl/
 
 On windows the easiest way is:
 
@@ -15,11 +16,11 @@ choco install kubernetes-helm
 # Install the cloud native operator
 .\operator\cloudnative\deploy.ps1
 
-# Install Prometheus and Grafana
-.\operator\Prometheus\deploy.ps1
-
 # Install keycloak
 .\operator\keycloak\deploy.ps1
+
+# Install Prometheus and Grafana
+.\operator\Prometheus\deploy.ps1
 
 # To access Prometheus, port-forward the Prometheus service
 kubectl port-forward svc/prometheus-community-kube-prometheus 9090
@@ -33,3 +34,8 @@ kubectl port-forward svc/prometheus-community-grafana 3000:80
 # access Prometheus locally at http://localhost:9090
 # access Grafana locally at http://localhost:3000/ providing the credentials 
 # admin as username, prom-operator as password (defined in kube-stack-config.yaml).
+
+minikube tunnel
+
+
+$ curl -kivL -H 'Host: http://keycloak.192.168.49.2.nip.io:8080/' 'http://127.0.0.1:8080'
